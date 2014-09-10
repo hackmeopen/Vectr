62092c6926149e7b480ab00de40a3e45781c0f4d
hackmeopen <matt@hackmeopen.com>
2014-09-10 13:09:59 -0500

    Working on quantization bug

--- Vectr_Firmware/quantization_tables.h
+++ Vectr_Firmware/quantization_tables.h
@@ -8,18 +8,18 @@
 #ifndef QUANTIZATION_TABLES_H
 #define	QUANTIZATION_TABLES_H
 
-#define LENGTH_OF_CHROMATIC_SCALE   120
-#define LENGTH_OF_MAJOR_SCALE       70
-#define LENGTH_OF_PENTATONIC_SCALE  50
-#define LENGTH_OF_OCTAVE_SCALE      10
+#define LENGTH_OF_CHROMATIC_SCALE   121
+#define LENGTH_OF_MAJOR_SCALE       71
+#define LENGTH_OF_PENTATONIC_SCALE  51
+#define LENGTH_OF_OCTAVE_SCALE      11
 
 
 extern const uint32_t u32QuantizedSpeedTable[15];
 extern const uint16_t u16LogTable[1024];
 extern const uint16_t u16ChromaticScale[LENGTH_OF_CHROMATIC_SCALE];
-extern const uint16_t u16MajorScale[70];
-extern const uint16_t u16PentatonicScale[50];
-const uint16_t u16OctaveScale[10];
+extern const uint16_t u16MajorScale[LENGTH_OF_MAJOR_SCALE];
+extern const uint16_t u16PentatonicScale[LENGTH_OF_PENTATONIC_SCALE];
+const uint16_t u16OctaveScale[LENGTH_OF_OCTAVE_SCALE];
 extern const uint32_t u32LogSpeedTable[	1024];
 #define LENGTH_OF_LOG_TABLE     1024
 #define BIT_SHIFT_TO_LOG_TABLE 6
